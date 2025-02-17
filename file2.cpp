74ef5640dc82a558ee987dd6949e9bd11ae9d361
20k <icedshot@gmail.com>
2025-02-17 08:25:25 +0000

    simplify the test case, we don't need a header for it to work

--- file2.cpp
+++ file2.cpp
@@ -1,5 +1,4 @@
 #include "common.hpp"
-#include "file2.hpp"
 
 void thing_doer()
 {
--- file2.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef HEADER_564C8646DFB9C41D
-#define HEADER_564C8646DFB9C41D
-
-void thing_doer();
-#endif // header guard 
-
